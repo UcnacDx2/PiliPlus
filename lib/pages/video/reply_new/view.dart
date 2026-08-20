57114d1c3db09050287a75c6bb41e6b37c29978d
UcnacDx2 <127503808+UcnacDx2@users.noreply.github.com>
2026-08-20 19:07:00 +0800

    fix(deps): align with primary upstream dependency set

--- lib/pages/video/reply_new/view.dart
+++ lib/pages/video/reply_new/view.dart
@@ -352,7 +352,7 @@ class _ReplyPageState extends CommonRichTextPubPageState<ReplyPage> {
                     final res = await plPlayerController
                         .plPlayerController
                         .videoPlayerController
-                        ?.screenshot(format: .png);
+                        ?.screenshot();
                     if (res != null) {
                       final path =
                           '$tmpDirPath/${Utils.generateRandomString(8)}.png';
