5861d71636410d9365c47fac4e1448631d65ba9b
UcnacDx2 <127503808+UcnacDx2@users.noreply.github.com>
2026-09-01 14:03:32 +0800

    fix(tv): keep system chrome import

--- lib/main_tv.dart
+++ lib/main_tv.dart
@@ -18,6 +18,7 @@ import 'package:PiliPlus/utils/storage_pref.dart';
 import 'package:PiliPlus/utils/utils.dart';
 import 'package:catcher_2/catcher_2.dart';
 import 'package:flutter/foundation.dart' show debugPrint, kDebugMode;
+import 'package:flutter/services.dart';
 import 'package:material_ui/material_ui.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
